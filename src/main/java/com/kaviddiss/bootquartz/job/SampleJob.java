21ab93f9e195261755bf91cf2de56137ce86edbc
cane <webcane@ya.ru>
2016-05-21 21:57:50 +0300

    PersistJobDataAfterExecution

--- src/main/java/com/kaviddiss/bootquartz/job/SampleJob.java
+++ src/main/java/com/kaviddiss/bootquartz/job/SampleJob.java
@@ -1,14 +1,20 @@
 package com.kaviddiss.bootquartz.job;
 
-import com.kaviddiss.bootquartz.service.SampleService;
+import org.quartz.DisallowConcurrentExecution;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
+import org.quartz.PersistJobDataAfterExecution;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kaviddiss.bootquartz.service.SampleService;
 
 /**
  * Created by david on 2015-01-20.
  */
+@PersistJobDataAfterExecution
+@DisallowConcurrentExecution
 public class SampleJob implements Job {
+	
     @Autowired
     private SampleService service;
 
