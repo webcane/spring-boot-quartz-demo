bf7f1699a101c4cc1678f02a2e7e306c6896b7c1
cane <webcane@ya.ru>
2016-05-21 21:55:52 +0300

    add h2 console

---
+++ src/main/java/com/kaviddiss/bootquartz/config/H2Config.java
@@ -0,0 +1,17 @@
+package com.kaviddiss.bootquartz.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class H2Config {
+
+    @Bean
+    ServletRegistrationBean h2servletRegistration(){
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean( new WebServlet());
+        registrationBean.addUrlMappings("/console/*");
+        return registrationBean;
+    }
+}
